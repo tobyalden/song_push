fcd0a131035f6c9614c48362e8481680e357cff3
Toby Alden <student@epicodus.com>
2015-06-04 16:10:12 -0700

    Finished pawn logic, minus promotion and en passant.

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,8 @@ function Piece(x, y, type, color) {
 
 Piece.prototype.isLegalMove = function(x, y) {
 
+  // debugger;
+
   var pieceAt = getPieceAt(x, y);
   if(pieceAt != null && pieceAt.color === this.color) {
     return false;
@@ -31,13 +33,15 @@ Piece.prototype.isLegalMove = function(x, y) {
     }
 
     if(x === this.x) {
-      if(this.y == startRow && y === this.y + 2 * colorMod) {
+      if(this.y == startRow && y === this.y + 2 * colorMod && getPieceAt(x, y) == null) {
         return true;
-      } else if(y === this.y + 1 * colorMod) {
+      } else if(y === this.y + 1 * colorMod && getPieceAt(x, y) == null) {
         return true;
       }
+    } else if((x === this.x + 1 || x === this.x - 1) && y === this.y + 1 * colorMod && getPieceAt(x, y) != null) {
+        return true;
     } else {
-      return false;
+        return false;
     }
 
   }
@@ -45,6 +49,25 @@ Piece.prototype.isLegalMove = function(x, y) {
 }
 
 pieces = []
+
+function setUpBoard() {
+  pieces.push(new Piece(0, 6, "pawn", "white"));
+  pieces.push(new Piece(1, 6, "pawn", "white"));
+  pieces.push(new Piece(2, 6, "pawn", "white"));
+  pieces.push(new Piece(3, 6, "pawn", "white"));
+  pieces.push(new Piece(4, 6, "pawn", "white"));
+  pieces.push(new Piece(5, 6, "pawn", "white"));
+  pieces.push(new Piece(6, 6, "pawn", "white"));
+  pieces.push(new Piece(7, 6, "pawn", "white"));
+  pieces.push(new Piece(0, 1, "pawn", "black"));
+  pieces.push(new Piece(1, 1, "pawn", "black"));
+  pieces.push(new Piece(2, 1, "pawn", "black"));
+  pieces.push(new Piece(3, 1, "pawn", "black"));
+  pieces.push(new Piece(4, 1, "pawn", "black"));
+  pieces.push(new Piece(5, 1, "pawn", "black"));
+  pieces.push(new Piece(6, 1, "pawn", "black"));
+  pieces.push(new Piece(7, 1, "pawn", "black"));
+}
 
 function getPieceAt(x, y) {
   for(var i = 0; i < pieces.length; i++) {
@@ -61,10 +84,7 @@ $(document).ready(function() {
 
   var selectedPiece = null;
 
-  pieces.push(new Piece(2, 6, "pawn", "white"));
-  pieces.push(new Piece(2, 4, "pawn", "white"));
-  pieces.push(new Piece(2, 1, "pawn", "black"));
-
+  setUpBoard();
   drawBoard();
 
   $("td").click(function(event) {
@@ -72,16 +92,17 @@ $(document).ready(function() {
     var x = parseInt(coords[0]);
     var y = parseInt(coords[1]);
 
-    // debugger;
-
     if(selectedPiece === null) {
       selectedPiece = getPieceAt(x, y);
     } else if(selectedPiece.isLegalMove(x, y)) {
-      selectedPiece.x = x;
-      selectedPiece.y = y;
-      selectedPiece = null;
+        if(getPieceAt(x, y) != null) {
+          pieces.splice(pieces.indexOf(getPieceAt(x, y)), 1);
+        }
+        selectedPiece.x = x;
+        selectedPiece.y = y;
+        selectedPiece = null;
     } else if(x === selectedPiece.x && y === selectedPiece.y) {
-      selectedPiece = null;
+        selectedPiece = null;
     } else {
 
     }
